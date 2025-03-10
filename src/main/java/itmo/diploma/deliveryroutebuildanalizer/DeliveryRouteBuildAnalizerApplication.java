af219cf3f25ad230f41470ad8c6fcfd0108df06a
arhip <rabokony@icloud.com>
2025-03-10 20:06:10 +0300

    initial project

---
+++ src/main/java/itmo/diploma/deliveryroutebuildanalizer/DeliveryRouteBuildAnalizerApplication.java
@@ -0,0 +1,13 @@
+package itmo.diploma.deliveryroutebuildanalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRouteBuildAnalizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryRouteBuildAnalizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itmo/diploma/deliveryroutebuildanalizer/DeliveryRouteBuildAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.diploma.deliveryroutebuildanalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryRouteBuildAnalizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
